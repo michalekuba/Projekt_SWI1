7c5306f17b5d8cd2054bfbd974c977238543d8d4
Jakub Michálek <michalekuba@JM-MacBook.local>
2026-04-27 12:50:00 +0200

    Vytvoření služby ProductService

---
+++ backend/src/main/java/com/osu/michja56/backend/service/ProductService.java
@@ -0,0 +1,30 @@
+package com.osu.michja56.backend.service;
+
+import com.osu.michja56.backend.model.Product;
+import com.osu.michja56.backend.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
