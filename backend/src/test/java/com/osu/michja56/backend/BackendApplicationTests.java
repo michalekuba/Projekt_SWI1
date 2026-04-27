a12255aea0f01d03cd958a80975ddcc0b47f302d
Jakub Michálek <michalekuba@JM-MacBook.local>
2026-04-27 11:36:39 +0200

    Initial commit: Nastavení struktury repozitáře (Spring Boot + React)

---
+++ backend/src/test/java/com/osu/michja56/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osu.michja56.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
