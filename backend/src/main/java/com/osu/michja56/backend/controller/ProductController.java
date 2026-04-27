1b755158c161613a17768382af2a8f3006d7ba80
Jakub Michálek <michalekuba@JM-MacBook.local>
2026-04-27 12:54:33 +0200

    Vytvoření REST API Controlleru

---
+++ backend/src/main/java/com/osu/michja56/backend/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.osu.michja56.backend.controller;
+
+import com.osu.michja56.backend.model.Product;
+import com.osu.michja56.backend.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@CrossOrigin(origins = "http://localhost:5173")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+}
